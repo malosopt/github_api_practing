7f790e712ec5ca7aa1af56f52cfce638e6859d9f
Miguel Angel <Miangel00.77@gmail.com>
2021-06-17 14:21:49 -0500

    Descarga el nombre de los repositorios creados en github -publicos-

--- get_repositorios.py
+++ get_repositorios.py
@@ -7,7 +7,7 @@ import json
 
 client_secret = '1035d0e424c9828e39d6b19f92a5ec82e3ea21c0'
 client_id = 'bf186ec51be5654b29df'
-access_token = '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'
+access_token = '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'
 
 # CURL instruccion
 #$ curl -H "Authorization: token OAUTH-TOKEN" https://api.github.com/user/repos
@@ -19,7 +19,7 @@ headers = {'Authorization':access_token_modifiy}
 ruta = 'https://api.github.com/user/repos'
 
 response = requests.get(ruta,headers=headers)
-print(response.status_code)
+print(f'status: {response.status_code}')
 
 #archivo-salida.py
 f = open ('archivo.json','w') #w write
@@ -27,11 +27,12 @@ f.write(response.text)
 f.close()
 
 response = response.json()
-#print(dir(response))
-#print(response.content)
-#print(response.text)
-print(type(response))
+# print(dir(response))
+# print(response.content)
+# print(response.text)
+# print(type(response))
 for project in response:
+    print('------------------')
     print(project['name'])
 
 
--- github_api_conexion.py
+++ github_api_conexion.py
@@ -1,6 +1,7 @@
 # Autenticacion Oauth - obtener access_token
 import requests
 import json
+
 # First Step:
 # Go to the github page
 # developer github oauth -- registrar app github Oauth
@@ -8,38 +9,37 @@ import json
 client_secret = '1035d0e424c9828e39d6b19f92a5ec82e3ea21c0'
 client_id = 'bf186ec51be5654b29df'
 
-# # ruta = 'https://github.com/login/oauth/authorize'
-# # payload = {'client_id':client_id }#,'client_secret':client_secret}
+ruta = 'https://github.com/login/oauth/authorize'
+payload = {'client_id':client_id }#,'client_secret':client_secret}
 
-# # respuesta = requests.get(ruta,payload)
-# # print(respuesta.status_code)
-# # print(respuesta.url)
+respuesta = requests.get(ruta,payload)
+print(respuesta.status_code)
+print(respuesta.url)
 
-# # code = input('Please input the code from url: ')
-# # #https://github.com/login?client_id=bf186ec51be5654b29df&return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id%3Dbf186ec51be5654b29df%26client_secret%3D1035d0e424c9828e39d6b19f92a5ec82e3ea21c0
-# # #Abrimos la url en el navegador, autorizamos el uso de la app
+code = input('Please input the code from url: ')
+#https://github.com/login?client_id=bf186ec51be5654b29df&return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id%3Dbf186ec51be5654b29df%26client_secret%3D1035d0e424c9828e39d6b19f92a5ec82e3ea21c0
+#Abrimos la url en el navegador, autorizamos el uso de la app
 
-# # #https://www.google.com.co/?code=a4eaffc3a260ec7fad1c
-# # #code='34d1bc56494f45795d66'
-# # # Obtenemos el code de la url a donde fuimos redirigidos luego de autorizar.
+#https://www.google.com.co/?code=a4eaffc3a260ec7fad1c
+#code='34d1bc56494f45795d66'
+# Obtenemos el code de la url a donde fuimos redirigidos luego de autorizar.
 
-# # #2. Step!
+#2. Step!
 
-# # payload = {'client_id':client_id,
-# #            'client_secret':client_secret,
-# #            'code':code}
-# # headers = {'Accept':'application/json'}
-# # ruta = 'https://github.com/login/oauth/access_token'
+payload = {'client_id':client_id,
+           'client_secret':client_secret,
+           'code':code}
+headers = {'Accept':'application/json'}
+ruta = 'https://github.com/login/oauth/access_token'
 
-# # respuesta = (requests.post(ruta,payload,headers=headers))
-# # print(respuesta.status_code)
-# # print(respuesta.json())
-# # respuesta_json = respuesta.json()
-# # print(respuesta_json['access_token'])
+respuesta = (requests.post(ruta,payload,headers=headers))
+print(respuesta.status_code)
+print(respuesta.json())
+respuesta_json = respuesta.json()
+print(respuesta_json['access_token'])
 
-# # # print(respuesta.text)
-# # # respuesta =  json.loads(respuesta.text)
-# # # print(type(respuesta))
-# # # print(respuesta['access_token'])
+# print(respuesta.text)
+# respuesta =  json.loads(respuesta.text)
+# print(type(respuesta))
+# print(respuesta['access_token'])
 
-access_token = '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'
